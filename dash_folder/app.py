68984e88a1aa334b363efa144c71e077a62391bc
Bjorn Are Therkelsen <ba.therk@outlook.com>
2021-10-29 10:52:05 +0900

    Add metrics for classification

--- dash_folder/app.py
+++ dash_folder/app.py
@@ -7,23 +7,29 @@ from utils.data_handling import load_data
 import numpy as np
 from dash.dependencies import Input, Output, State
 from dash_folder.template import assets_folder, colors
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 app = dash.Dash(__name__, assets_folder=assets_folder)
 
 df = load_data('latent_classification')
 X_START = 0.02
 delta = 0.2
-yy, zz = np.meshgrid(np.arange(df['y'].min(), df['y'].max(), delta), np.arange(df['z'].min(), df['z'].max(), delta))
+x = df['x'].to_numpy()
+y = df['y'].to_numpy()
+z = df['z'].to_numpy()
+labels = df['label'].to_numpy()
+
+yy, zz = np.meshgrid(np.arange(x.min(), y.max(), delta), np.array([z.min(), z.max()]))
 xx = np.ones(yy.shape)
 
 points = go.Scatter3d(
-        x=df['x'],
-        y=df['y'],
-        z=df['z'],
+        x=x,
+        y=y,
+        z=z,
         mode='markers',
         marker=dict(
             size=5,
-            color=[colors.yellow if label else colors.blue for label in df['label']],
+            color=[colors.yellow if label else colors.blue for label in labels],
             opacity=1)
         )
 
@@ -43,8 +49,18 @@ fig = go.Figure(
 app.layout = html.Div(
     children=[
         html.H1(children='Latent Visualizer'),
-        dcc.Graph(id='graph', figure=fig),
-        dcc.Slider(id='plane', min=df['x'].min(), max=df['x'].max(), step=0.01, value=X_START, marks={0.02: 'SVM'}, updatemode='drag')
+        html.Div([
+            html.Div([
+                html.Div(id='accuracy'),
+                html.Div(id='precision'),
+                html.Div(id='recall'),
+            ], id='metrics',
+                style={'display':'flex', 'width':'30%'}),
+            dcc.Graph(id='graph', figure=fig),
+
+        ]),
+
+        dcc.Slider(id='plane', min=x.min(), max=x.max(), step=delta, value=X_START, marks={0.02: 'SVM'}, updatemode='drag')
     ]
 )
 
@@ -56,7 +72,12 @@ def copy_layout(layout, relayout_data):
             layout.scene.camera.update(relayout_data['scene.camera'])
     return layout
 
-@app.callback(Output('graph','figure'), Input('plane','value'), State("graph", "relayoutData"))
+@app.callback(Output('graph','figure'),
+              Output('accuracy','children'),
+              Output('precision','children'),
+              Output('recall','children'),
+              Input('plane','value'),
+              State("graph", "relayoutData"))
 def change_plane_x(slider_value, relayout_data):
     plane=go.Surface(
             x=xx*slider_value,
@@ -66,10 +87,13 @@ def change_plane_x(slider_value, relayout_data):
             opacity=0.8,
             showscale=False
         )
+    predicted = np.where(x < slider_value, 0,1)
+    accuracy = accuracy_score(labels, predicted)
+    precision = precision_score(labels, predicted)
+    recall = recall_score(labels, predicted)
     layout = go.Layout()
     layout = copy_layout(layout, relayout_data)
-    print(relayout_data)
-    return go.Figure(data=[points,plane], layout=layout)
+    return go.Figure(data=[points,plane], layout=layout), accuracy, precision, recall
 
 if __name__ == '__main__':
     app.run_server(debug=True)
