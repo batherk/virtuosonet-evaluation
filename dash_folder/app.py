567286ede3a3ceb3252febf599d3f80650d1739a
Bjorn Are Therkelsen <ba.therk@outlook.com>
2021-10-29 12:25:45 +0900

    Add labels for sad and anger

--- dash_folder/app.py
+++ dash_folder/app.py
@@ -19,41 +19,66 @@ y = df['y'].to_numpy()
 z = df['z'].to_numpy()
 labels = df['label'].to_numpy()
 
+anger_mask = df['label']==0
+sad_mask = df['label']==1
+
 yy, zz = np.meshgrid(np.arange(y.min(), y.max(), delta), np.array([z.min(), z.max()]))
 xx = np.ones(yy.shape)
 
-points = go.Scatter3d(
-        x=x,
-        y=y,
-        z=z,
-        mode='markers',
-        marker=dict(
-            size=5,
-            color=[colors.yellow if label else colors.blue for label in labels],
-            opacity=1)
-        )
+anger_samples = go.Scatter3d(
+    x=df[anger_mask]['x'],
+    y=df[anger_mask]['y'],
+    z=df[anger_mask]['z'],
+    name='Anger',
+    mode='markers',
+    marker=dict(
+        size=5,
+        color=colors.yellow,
+        opacity=1
+    )
+)
+
+sad_samples = go.Scatter3d(
+    x=df[sad_mask]['x'],
+    y=df[sad_mask]['y'],
+    z=df[sad_mask]['z'],
+    name='Sad',
+    mode='markers',
+    marker=dict(
+        size=5,
+        color=colors.blue,
+        opacity=1
+    )
+)
 
 plane = go.Surface(
-            x=xx*X_START,
-            y=yy,
-            z=zz,
-            colorscale=['grey' for i in range(len(xx))],
-            opacity=0.8,
-            showscale=False
-        )
+    x=xx*X_START,
+    y=yy,
+    z=zz,
+    colorscale=['grey' for i in range(len(xx))],
+    opacity=0.8,
+    showscale=False,
+    name='Classification Plane',
+    showlegend=True
+)
 
 
 graph_layout_default = go.Layout()
-graph_layout_default_settings = {'scene.camera': {
-    'up': {'x': 0, 'y': 0, 'z': 1},
-    'center': {'x': -0.07214567600111127, 'y': -0.08497201560137722, 'z': -0.27943759806176177},
-    'eye': {'x': -0.5135394958253185, 'y': -1.9748036183855688, 'z': 0.7264046470993168},
-    'projection': {'type': 'perspective'}
+graph_layout_default_settings = {'scene': {
+    'camera': {
+        'up': {'x': 0, 'y': 0, 'z': 1},
+        'center': {'x': -0.07214567600111127, 'y': -0.08497201560137722, 'z': -0.27943759806176177},
+        'eye': {'x': -0.5135394958253185, 'y': -1.9748036183855688, 'z': 0.7264046470993168},
+        'projection': {'type': 'perspective'},
+    },
+    'xaxis_title': 'Anger(-),Sad(+)-Dim',
+    'yaxis_title': 'PCA 1',
+    'zaxis_title': 'PCA 2',
 }}
 graph_layout_default.update(graph_layout_default_settings)
 
 graph_fig = go.Figure(
-    data=[points, plane],
+    data=[anger_samples, sad_samples, plane],
     layout=graph_layout_default
 
 )
@@ -119,7 +144,7 @@ def change_plane_x(slider_value, relayout_data):
     recall = recall_score(labels, predicted)
     layout = graph_layout_default
     layout = copy_relayout_data(layout, relayout_data)
-    return go.Figure(data=[points,plane], layout=layout), accuracy, precision, recall
+    return go.Figure(data=[anger_samples, sad_samples,plane], layout=layout), accuracy, precision, recall
 
 if __name__ == '__main__':
     app.run_server(debug=True)
