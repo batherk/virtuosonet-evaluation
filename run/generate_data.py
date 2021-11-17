a388efced69a9c938e8c049323d6896204f8af87
Bjorn Are Therkelsen <ba.therk@outlook.com>
2021-11-17 15:30:40 +0900

    Remove files related to old virtuosoNet

--- run/generate_data.py
+++
@@ -1,5 +0,0 @@
-from utils.data_generation import generate_multiple_styles_df
-from utils.data_handling import save_data
-
-styles_df = generate_multiple_styles_df(amount=100)
-save_data(styles_df, name='all_styles_100')
--- run/plot_latents.py
+++
@@ -1,5 +0,0 @@
-from utils.data_handling import load_data
-from utils.plot import plot_latents
-
-styles_df = load_data(name='multiple_styles')
-plot_latents(styles_df)
